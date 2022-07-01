3aa7521987e7119880599d1f8062dd738a80a0f1
tanapat-Frevation <tanapat@frevation.com>
2022-07-01 15:08:53 +0700

    update api 20220701

--- create-debit-notes.js
+++ create-debit-notes.js
@@ -4,24 +4,31 @@ const URl = 'https://api.leceipt.com';
 const endpoint = '/etax/documents/debit-notes';
 const API_Key = 'YOUR-API-KEY';
 
-let company = {
-  name: 'บริษัท ผู้ขายทดสอบ จำกัด',
-  buildingNumber: '999',
-  address: 'หมู่ 999',
-  streetPrefix: 'ถ.',
-  streetName: 'สาทร 999',
-  subDistrictPrefix: 'แขวง',
-  subDistrictCode: '100402',
-  subDistrictName: 'สีลม',
-  districtPrefix: 'เขต',
-  districtCode: '1004',
-  districtName: 'บางรัก',
-  provincePrefix: '',
-  provinceCode: '10',
-  provinceName: 'กรุงเทพมหานคร',
-  postcode: '10500',
+// unstructure เป็น true
+// let customer = {
+//   name: 'บริษัท ลูกค้าทดสอบ จำกัด',
+//   addressLineOne: '999 หมู่ 999 ถ.สาทร 99',
+//   addressLineTwo: 'แขวงสีลม เขตบางรัก กรุงเทพมหานคร',
+//   postcode: '10500',
+//   branchNumber: '00000',
+//   taxNumberType: 'TXID',
+//   taxId: '1234567890123',
+//   phone: '0812345678',
+//   email: 'test@test.com',
+//   unstructure: true,
+// };
+
+// unstructure เป็น false
+let customer = {
+  name: 'บริษัท ลูกค้าทดสอบ จำกัด',
+  buildingNumber: '99',
+  address: 'หมู่ที่ 99',
+  streetName: 'สาทร 99',
   branchNumber: '00000',
-  branchText: '(สำนักงานใหญ่)',
+  subDistrictCode: '100402',
+  districtCode: '1004',
+  provinceCode: '10',
+  postcode: '10500',
   taxNumberType: 'TXID',
   taxId: '1234567890123',
   phoneCountryCode: '+66',
@@ -30,79 +37,46 @@ let company = {
   unstructure: false,
 };
 
-let customer = {
-  name: 'บริษัท ลูกค้าทดสอบ จำกัด',
-  addressLineOne: '999 หมู่ 999 ถ.สาทร 99',
-  addressLineTwo: 'แขวงสีลม เขตบางรัก กรุงเทพมหานคร',
-  postcode: '10500',
-  branchNumber: '00000',
-  branchText: '(สำนักงานใหญ่)',
-  taxNumberType: 'TXID',
-  taxId: '1234567890123',
-  phoneCountryCode: '+66',
-  phone: '0812345678',
-  email: 'test@test.com',
-  unstructure: true,
-};
-
 let items = [
   {
     number: 1,
     description: 'ค่าอาหารและเครื่องดื่ม',
     quantity: 1,
-    unitCode: '',
-    unitName: '',
-    price: 10000,
+    unitCode: 'EA',
+    price: 1500,
     discount: 0,
-    percentVat: 7,
-    percentVatText: '',
-    total: 10000,
+    percentVat: 0,
   },
   {
     number: 2,
     description: 'ค่าอาหารและเครื่องดื่ม',
-    quantity: 2,
+    quantity: 1,
     unitCode: '',
-    unitName: '',
-    price: 4000,
+    price: 1000,
     discount: 0,
     percentVat: 7,
-    percentVatText: '',
-    total: 8000,
   },
 ];
 
 let refer = {
-  typeCode: '388',
-  typeName: 'ใบกำกับภาษี',
   number: 'TIV20210300000',
-  date: '2021-03-17T17:00:00.000Z',
   dateBE: '18/03/2564',
-  reasonCode: 'TIVC01',
-  reasonName: 'ชื่อผิด',
-  amountTotal: 5000,
-  diffAmountTotal: 13000,
+  reasonCode: 'DBNG01',
+  typeCode: 'T02',
+  amountTotal: 2000,
 };
 
 let data = {
-  company: company, // company จากข้อมูลก่อนหน้า
-  customer: customer, // customer จากข้อมูลก่อนหน้า
-  date: '2021-03-23T14:50:21.073Z',
-  dateBE: '23/03/2564',
-  dateCE: '23/03/2021',
-  items: items, // items จากข้อมูลก่อนหน้า
-  note: 'ทดสอบหมายเหตุ',
   number: 'TIV20210300001',
-  receivedBy: 'สมชาย',
-  percentVat: 7,
-  amountTotal: 18000,
-  nonVatPriceTotal: 0,
-  zeroVatPriceTotal: 0,
-  vatPriceTotal: 13000,
-  vatTotal: 910,
-  grandTotal: 13910,
+  dateBE: '23/03/2564',
+  calculation_method: 2,
+  includeVat: false,
+  items: items, // items จากข้อมูลก่อนหน้า
   discount: 0,
-  reIssue: true,
+  language: 'th',
+  customer: customer, // customer จากข้อมูลก่อนหน้า
+  note: 'ทดสอบหมายเหตุ',
+  receivedBy: 'สมชาย',
   refer: refer, // refer จากข้อมูลก่อนหน้า
 };
 
--- create-taxinvoice.js
+++ create-taxinvoice.js
@@ -4,24 +4,32 @@ const URl = 'https://api.leceipt.com';
 const endpoint = '/etax/documents/taxinvoices';
 const API_Key = 'YOUR-API-KEY';
 
-let company = {
-  name: 'บริษัท ผู้ขายทดสอบ จำกัด',
-  buildingNumber: '999',
-  address: 'หมู่ 999',
-  streetPrefix: 'ถ.',
-  streetName: 'สาทร 999',
-  subDistrictPrefix: 'แขวง',
-  subDistrictCode: '100402',
-  subDistrictName: 'สีลม',
-  districtPrefix: 'เขต',
-  districtCode: '1004',
-  districtName: 'บางรัก',
-  provincePrefix: '',
-  provinceCode: '10',
-  provinceName: 'กรุงเทพมหานคร',
-  postcode: '10500',
+// unstructure เป็น true
+
+// let customer = {
+//   name: 'บริษัท ลูกค้าทดสอบ จำกัด',
+//   addressLineOne: '999 หมู่ 999 ถ.สาทร 99',
+//   addressLineTwo: 'แขวงสีลม เขตบางรัก กรุงเทพมหานคร',
+//   postcode: '10500',
+//   branchNumber: '00000',
+//   taxNumberType: 'TXID',
+//   taxId: '1234567890123',
+//   phone: '0812345678',
+//   email: 'test@test.com',
+//   unstructure: true,
+// };
+
+// unstructure เป็น false
+let customer = {
+  name: 'บริษัท ลูกค้าทดสอบ จำกัด',
+  buildingNumber: '99',
+  address: 'หมู่ที่ 99',
+  streetName: 'สาทร 99',
   branchNumber: '00000',
-  branchText: '(สำนักงานใหญ่)',
+  subDistrictCode: '100402',
+  districtCode: '1004',
+  provinceCode: '10',
+  postcode: '10500',
   taxNumberType: 'TXID',
   taxId: '1234567890123',
   phoneCountryCode: '+66',
@@ -30,65 +38,44 @@ let company = {
   unstructure: false,
 };
 
-let customer = {
-  name: 'บริษัท ลูกค้าทดสอบ จำกัด',
-  addressLineOne: '999 หมู่ 999 ถ.สาทร 99',
-  addressLineTwo: 'แขวงสีลม เขตบางรัก กรุงเทพมหานคร',
-  postcode: '10500',
-  branchNumber: '00000',
-  branchText: '(สำนักงานใหญ่)',
-  taxNumberType: 'TXID',
-  taxId: '1234567890123',
-  phoneCountryCode: '+66',
-  phone: '0812345678',
-  email: 'test@test.com',
-  unstructure: true,
-};
-
 let items = [
   {
     number: 1,
     description: 'ค่าอาหารและเครื่องดื่ม',
     quantity: 1,
-    unitCode: '',
-    unitName: '',
+    unitCode: 'EA',
     price: 1500,
     discount: 0,
+    percentVat: 0,
+  },
+  {
+    number: 2,
+    description: 'ค่าอาหารและเครื่องดื่ม',
+    quantity: 1,
+    unitCode: '',
+    price: 1000,
+    discount: 0,
     percentVat: 7,
-    percentVatText: '',
-    total: 1500,
   },
 ];
 
 let refer = {
   // สามารถเว้นว่างได้ ถ้า reIssue เป็น false
-  typeCode: '388',
-  typeName: 'ใบกำกับภาษี',
   number: 'TIV20210300000',
-  date: '2021-03-17T17:00:00.000Z',
   dateBE: '18/03/2564',
   reasonCode: 'TIVC01',
-  reasonName: 'ชื่อผิด',
 };
 
 let data = {
-  company: company, // company จากข้อมูลก่อนหน้า
-  customer: customer, // customer จากข้อมูลก่อนหน้า
-  date: '2021-03-23T14:50:21.073Z',
-  dateBE: '23/03/2564',
-  dateCE: '23/03/2021',
-  items: items, // items จากข้อมูลก่อนหน้า
-  note: 'ทดสอบหมายเหตุ',
   number: 'TIV20210300001',
-  receivedBy: 'สมชาย',
-  percentVat: 7,
-  amountTotal: 10000,
-  nonVatPriceTotal: 0,
-  zeroVatPriceTotal: 0,
-  vatPriceTotal: 10000,
-  vatTotal: 700,
-  grandTotal: 10700,
+  dateBE: '23/03/2564',
+  includeVat: false,
+  items: items, // items จากข้อมูลก่อนหน้า
   discount: 0,
+  language: 'th',
+  customer: customer, // customer จากข้อมูลก่อนหน้า
+  note: 'ทดสอบหมายเหตุ',
+  receivedBy: 'สมชาย',
   reIssue: true,
   refer: refer, // refer จากข้อมูลก่อนหน้า
 };
