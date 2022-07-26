a98498d93daa514a3f866978268da9d6d92ac0f1
tanapat-Frevation <tanapat@frevation.com>
2022-07-26 10:58:55 +0700

    receipt-taxinvoice api update

--- create-receipt-taxinvoice.js
+++ create-receipt-taxinvoice.js
@@ -5,37 +5,36 @@ const endpoint = '/etax/documents/receipts-taxinvoices?api-version=2022-07-12';
 const API_Key = 'YOUR-API-KEY';
 
 // unstructure เป็น true
-
-// let customer = {
-//   name: 'บริษัท ลูกค้าทดสอบ จำกัด',
-//   addressLineOne: '999 หมู่ 999 ถ.สาทร 99',
-//   addressLineTwo: 'แขวงสีลม เขตบางรัก กรุงเทพมหานคร',
-//   postcode: '10500',
-//   branchNumber: '00000',
-//   taxNumberType: 'TXID',
-//   taxId: '1234567890123',
-//   phone: '0812345678',
-//   email: 'test@test.com',
-//   unstructure: true,
-// };
-
-// unstructure เป็น false
 let customer = {
   name: 'บริษัท ลูกค้าทดสอบ จำกัด',
-  buildingNumber: '99',
-  address: 'หมู่ที่ 99',
-  streetName: 'สาทร 99',
-  branchNumber: '00000',
-  subDistrictCode: '100402',
-  districtCode: '1004',
-  provinceCode: '10',
+  addressLineOne: '999 หมู่ 999 ถ.สาทร 99',
+  addressLineTwo: 'แขวงสีลม เขตบางรัก กรุงเทพมหานคร',
   postcode: '10500',
+  branchNumber: '00000',
   taxNumberType: 'TXID',
   taxId: '1234567890123',
   phone: '0812345678',
   email: 'test@test.com',
-  unstructure: false,
+  unstructure: true,
 };
+
+// unstructure เป็น false
+// let customer = {
+//   name: 'บริษัท ลูกค้าทดสอบ จำกัด',
+//   buildingNumber: '99',
+//   address: 'หมู่ที่ 99',
+//   streetName: 'สาทร 99',
+//   branchNumber: '00000',
+//   subDistrictCode: '100402',
+//   districtCode: '1004',
+//   provinceCode: '10',
+//   postcode: '10500',
+//   taxNumberType: 'TXID',
+//   taxId: '1234567890123',
+//   phone: '0812345678',
+//   email: 'test@test.com',
+//   unstructure: false,
+// };
 
 let items = [
   {
@@ -75,20 +74,26 @@ let data = {
   customer: customer, // customer จากข้อมูลก่อนหน้า
   note: 'ทดสอบหมายเหตุ',
   receivedBy: 'สมชาย',
-  reIssue: false,
+  reIssue: true,
   refer: refer, // refer จากข้อมูลก่อนหน้า
   reference: '',
 };
 
 async function CreateReceiptTaxInvoice() {
-  const response = await axios.post(URl + endpoint, data, {
-    headers: {
-      'API-Key': API_Key,
-      'Content-Type': 'application/json',
-    },
-  });
+  try {
+    const response = await axios.post(URl + endpoint, data, {
+      headers: {
+        'API-Key': API_Key,
+        'Content-Type': 'application/json',
+      },
+    });
 
-  return response.data;
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error.response.data);
+    return error.response.data;
+  }
 }
 
-console.log(CreateReceiptTaxInvoice());
+CreateReceiptTaxInvoice();
